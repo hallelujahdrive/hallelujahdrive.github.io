7b7925db417d3a27e792857678785c473b8dfbf9
hallelujahdrive <hallelujahdrive@gmail.com>
2021-02-24 02:53:53 +0900

    update elm-accordion to version 1.0.4

--- elm-accordion/demo.ts
+++ elm-accordion/demo.ts
@@ -4,6 +4,6 @@ import "elm-accordion/dist/elm-accordion.min.css";
 
 import Elm from "./src/Main.elm";
 
-const app = Elm.Main.init ({
-    node: document.getElementById("elm")!
+Elm.Main.init({
+  node: document.getElementById("elm")!,
 });
